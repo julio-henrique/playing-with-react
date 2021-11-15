6bae97f0250054aee64fcbd3db6555225507eff9
Julio Henrique <juliohjesus@gmail.com>
2021-11-15 08:45:52 -0300

    commit

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles.module.scss'
-
-export function Loading() {
-    return (
-        <div className={styles.loading}>
-            <h1>Loading...</h1>
-        </div>
-    )
-}
