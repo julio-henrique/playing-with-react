078c410f456a255f56174958d8b504a1b716368d
Julio Henrique <juliohjesus@gmail.com>
2021-11-16 21:19:58 -0300

    b-day-start

---
+++ birthday-reminder/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import data from './data';
+import List from './List';
+
+interface ListChildProps {
+  people: {}
+}
+
+interface ListChild {
+  people: ListChildProps[] 
+  id: number
+  name: string
+  image: string
+  age: number
+  length: () => void
+}
+
+function App() {
+  const [ people, setPeople ] = useState<ListChild[]>(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
+        {/* <button onClick={() => setPeople([])}>Clear All</button> */}
+      </section>
+    </main>
+  )
+}
+
+export default App;
