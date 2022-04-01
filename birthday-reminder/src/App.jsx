f8b83ba7557627d88752fcdd4eeb35c659a97780
Julio Henrique <juliohjesus@gmail.com>
2022-03-31 23:27:56 -0300

    doing birthday 2

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -7,8 +7,7 @@ function App() {
     <main>
       <section className='container'>
         <h3>{people.length} Birthdays Today</h3>
-        <List people={people} />
-        {/* <button onClick={() => setPeople([])}>Clear All</button> */}
+        
       </section>
       <a href="" target="_blank"></a>
     </main>
--- birthday-reminder/src/List.jsx
+++ birthday-reminder/src/List.jsx
@@ -1,10 +1,17 @@
 const List = () => {
   return (
-    <>
-      people
-      {}
-    </>
+    <h3>list</h3>
   );
 };
+
+/**
+ * <article className='person'>
+                        <img />
+                        <div>
+                        <h4>"name"</h4>
+                        <p> years</p>
+                        </div>
+                    </article>
+*/
 
 export default List;
