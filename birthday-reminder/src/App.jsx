57c35be8e2de7610521d79dd4af4ba3937af319a
Julio Henrique <juliohjesus@gmail.com>
2022-03-31 23:15:44 -0300

    doing birthday

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react';
 import data from './data';
-import List from './List';
-
-
+// import List from './List';
 
 function App() {
-  const [ people, setPeople ] = useState(data)
   return (
     <main>
       <section className='container'>
