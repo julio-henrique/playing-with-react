37b7815241533a467a7dc87339c328ee3abebf7e
Julio Henrique <juliohjesus@gmail.com>
2022-04-08 21:30:42 -0300

    doing tour

--- tour/src/hooks/useTour.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-export function TourProvider() {}
-
-export function useTour() {
-    const context = useContext
-}
--- tour/src/services/api.ts
+++
@@ -1,3 +0,0 @@
-import axios from "axios"
-
-export const api = 'https://course-api.com/react-tours-project'
