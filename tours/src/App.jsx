11e8517636f9ec8ecfc5113bd6910b063a4fe006
Julio Henrique <juliohjesus@gmail.com>
2022-04-11 17:20:47 -0300

    doing tours

---
+++ tours/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import { Loading } from './Loading'
+
+import './App.css'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+    const [loading, setLoading] = useState(true)
+    const [tours, setTours] = useState([])
+    
+  // Crie uma função para remover os tours
+
+
+    const fetchTours = async () => {
+        try {
+            const res = await fetch(url)
+            const data = res.json()
+            setLoading(false)
+            setTours(data)
+        } catch (error) {
+            setLoading(false)
+            console.log(error)
+        }
+    }
+
+    /* useEffect(() => {
+        fetchTours()
+    }, []) */
+
+    if(loading) return <Loading />
+
+
+  /**
+   * Se a tours.length for igual a 0
+   * então mostre a página abaixo
+    <main>
+      <div className='title'>
+      <h2>no tours left</h2>
+      <button className='btn'></button>
+      </div>
+    </main> 
+   */
+
+  // No final retorne o ToursIndex
+  return (
+    <main>
+    </main>
+  )
+}
+
+export default App
---
+++ tours/src/Loading.jsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+    return (
+        <main>
+            <div className="loading">
+                <h1>Loading...</h1>
+            </div>
+        </main>
+    )
+}
